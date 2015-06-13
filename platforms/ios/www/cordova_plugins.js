896ba75262d72f9f58a2fb818e19284a6224427c
David Lester <DML@Davids-MacBook-Air-2.local>
2015-06-13 11:13:43 -0700

    removing plugins

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,17 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.danielcwilson.plugins.googleanalytics/www/analytics.js",
-        "id": "com.danielcwilson.plugins.googleanalytics.UniversalAnalytics",
-        "clobbers": [
-            "analytics"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "com.danielcwilson.plugins.googleanalytics": "0.7.0"
-}
+{}
 // BOTTOM OF METADATA
 });

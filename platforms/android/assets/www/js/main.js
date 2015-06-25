1b6748236895bc0095827a83f5cc466922c74391
David Lester <DML@Davids-MacBook-Air-2.local>
2015-06-25 13:57:54 -0700

    removing vertical scrolling

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -30,8 +30,7 @@ $('.reviews, .reviews-overlay, .reviews-close, .nearby-search, .rate-app').hide(
 		$('.reviews-close').css('top', '35px');
 		$('#site-wrapper').css('padding-top', '74px');
 		$('.refresh').css('bottom', '74px');
-		$('.reviews').css('top', '-74px');
-		$('.reviews').css('padding', '74px 5% 5% 5%');
+		$('.reviews').css('height', 'calc(100% + 74px)');
 	} else {
 	    $('header').css('height', '54px');
 	    $('header').css('padding', '15px');
@@ -40,8 +39,7 @@ $('.reviews, .reviews-overlay, .reviews-close, .nearby-search, .rate-app').hide(
 		$('.reviews-close').css('top', '15px');
 		$('#site-wrapper').css('padding-top', '54px');
 		$('.refresh').css('bottom', '54px');
-		$('.reviews').css('top', '-54px');
-		$('.reviews').css('padding', '54px 5% 5% 5%');
+		$('.reviews').css('height', 'calc(100% + 54px)');
 	}
 	
 	/* OFF-CANVAS MENUS */

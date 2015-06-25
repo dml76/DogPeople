302d94a0a140d171f96f4908b6fd7d8296ae8990
David Lester <DML@Davids-MacBook-Air-2.local>
2015-06-25 14:48:22 -0700

    removing vertical scrolling

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -19,7 +19,7 @@ $('.reviews, .reviews-overlay, .reviews-close, .nearby-search, .rate-app').hide(
 		$('.reviews-close').css('top', '35px');
 		$('#site-wrapper').css('padding-top', '74px');
 		$('.refresh').css('bottom', '74px');
-		//$('.reviews').css('height', 'calc(100% - 74px)');
+		$('.container, .reviews').css('height', 'calc(100% - 74px)');
 	} else {
 	    $('header').css('height', '54px');
 	    $('header').css('padding', '15px');
@@ -28,7 +28,7 @@ $('.reviews, .reviews-overlay, .reviews-close, .nearby-search, .rate-app').hide(
 		$('.reviews-close').css('top', '15px');
 		$('#site-wrapper').css('padding-top', '54px');
 		$('.refresh').css('bottom', '54px');
-		//$('.reviews').css('height', 'calc(100% - 54px)');
+		$('.container, .reviews').css('height', 'calc(100% - 54px)');
 	}
 	
 	/* OFF-CANVAS MENUS */
